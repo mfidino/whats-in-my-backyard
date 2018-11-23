16ce4d32340322b72599c6e9ed898e0cc2c32188
Mason Fidino <mfidino@lpzoo.org>
2018-11-23 17:34:19 -0600

    added interactive  button

--- themes/winby/source/site.js
+++ themes/winby/source/site.js
@@ -1,1 +1,22 @@
-console.log("sup boi");
+function startSite() {
+  $("#map-trigger-button").on("click", function() {
+    updateSelectedNeighborhood();
+  });
+}
+$(document).ready(startSite);
+
+function updateSelectedNeighborhood() {
+  fetch("/species.json")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      replaceCurrentSpecies(data);
+    });
+}
+
+function replaceCurrentSpecies(data) {
+  $("#most-common-species-text").text(data.most_common.species);
+  $("#selected-neighborhood").text(data.neighborhood);
+  window.species = data;
+}
