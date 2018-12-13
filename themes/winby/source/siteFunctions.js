52ab9db3d8110d15ca4a6840ba92cc14021e2bcc
sethherr <seth.william.herr@gmail.com>
2018-12-12 18:03:39 -0800

    include my name in the comment

--- themes/winby/source/siteFunctions.js
+++ themes/winby/source/siteFunctions.js
@@ -5,7 +5,7 @@ class SiteFunctions {
 }
 
 // Lame way of dealing with module.exports (which we need for mocha) in scripts we load in the HTML
-// There's 100% a better way of doing this, but done trying to figure out a better *easier* way
+// There's 100% a better way of doing this, but done trying to figure out a better *easier* way - Seth
 var module;
 if (module) {
   module.exports = SiteFunctions;
