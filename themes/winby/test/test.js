34c2a7898880dfbd8484973f01ec0d245d4bf01e
Mason Fidino <mfidino@lpzoo.org>
2018-12-15 08:29:01 -0600

    added tests for aOrAn

--- themes/winby/test/test.js
+++ themes/winby/test/test.js
@@ -3,12 +3,23 @@ var SiteFunctions = require("../source/siteFunctions");
 var siteFunctions = new SiteFunctions();
 
 describe("makeUL", function() {
-  it("should return number of characters in a string", function() {
+  it("should return an html block for an unordered list given an array of species names", function() {
     var target = "<ul><li>raccoon</li><li>deer</li></ul>";
     assert.equal(siteFunctions.makeUL(["raccoon", "deer"]), target);
     var target2 = "<ul><li>raccoon</li></ul>";
     assert.equal(siteFunctions.makeUL(["raccoon"]), target2);
     var target3 = "<ul><li>raccoon</li><li>deer</li><li>coyote</li></ul>";
     assert.equal(siteFunctions.makeUL(["raccoon", "deer", "coyote"]), target3);
+  });
+});
+
+describe("aOrAn", function() {
+  it("should return 'an' if a number is between 80-89", function() {
+    var target1 = "an";
+    assert.equal(siteFunctions.aOrAn([81, 75, 90]), target1);
+    var target2 = "a";
+    assert.equal(siteFunctions.aOrAn([70, 45, 97]), target2);
+    var target3 = "an";
+    assert.equal(siteFunctions.aOrAn([80.32, 65, 98]), target3);
   });
 });
